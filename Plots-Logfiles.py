92b9e376f32eaf853d1f31f31231cbd0581cc9cb
Ali Abou Taka <aaboutaka@ucmerced.edu>
2020-05-15 16:01:41 -0700

    Added markers to the plos

--- Plots-Logfiles.py
+++ Plots-Logfiles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 # CREATED BY ALI ABOU TAKA
-# THIS PYTHON PROGRAM  HAS DIFFERENT FUNCTION THAT CAN BE USED
+# THIS PYTHON PROGRAM  HAS DIFFERENT FUNCTIONS THAT CAN BE USED
 # TO EXTRACT DESIRED INFORMATION FOR LOG FILES
 # BE CAREFULL WHAT FUNCTION YOU CALL AND WHAT PATH YOU PROVIDE. 
 
@@ -19,17 +19,16 @@ import matplotlib.style as style
 import os
 import sys
 
-####
-if len(sys.argv) < 2 or  len(sys.argv) > 2 :
-    print('You need  two arguments: name of the file and the path to the folder')
-    sys.exit(0)
-####    
 ##################################################
 # This Function will grab the energy at each cycle
 ##################################################
 def GrabEnrg(directory):
     filenameL=[]
-    for filename in os.listdir(directory):      
+    filenameLL=[]
+    x_keys =  []
+    y_values= []
+    markers=[4,5,6,7,'s','P', '^',"*",'X']
+    for filename in os.listdir(directory):
         if filename.endswith(".log"):
             filenameL.append(filename)
     for file in filenameL:
@@ -43,28 +42,31 @@ def GrabEnrg(directory):
                         energyval=float(words[1])
                         EnergyL.append(energyval)
         #     print (EnergyL)
-        xEnergyL=list(range(len(EnergyL)))
-        plt.plot(xEnergyL,EnergyL,label=os.path.splitext(os.path.split(file)[1])[0])     
-#         style.use('seaborn-poster') #sets the size of the charts
-        style.use('seaborn-talk')
-#         style.use('ggplot')
-        plt.xlabel('Number of SCF iterations')
-        plt.ylabel('Energy in (a.u.)')
-#       Making a small box under the plot for the legend
-        ax = plt.subplot(111)   
-        box = ax.get_position()    
-        ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                       box.width, box.height * 0.9])
+            x_keys.append(list(range(len(EnergyL))))
+#                 print(x_keys)
+            y_values.append(EnergyL)
+            filenameLL.append(os.path.splitext(os.path.split(file)[1])[0])
 
-#       Put a legend below current axis
+        zipped_list= list(zip(x_keys,y_values,markers,filenameLL))
+#         print(zipped3)
+    for x,y,z,fn in zipped_list:
+#         ax.plot(i,j, linestyle = '', marker=next(markers))
+        plt.plot(x,y,label=fn,marker=z,linewidth=0.5)
+        style.use('seaborn-talk')
+        ax = plt.subplot(111)
+        box = ax.get_position()
+        ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                         box.width, box.height * 0.9])
+        ax.set_xlabel('Number of SCF iterations')
+        ax.set_ylabel('Energy in (a.u.)')
+
+        # Put a legend below current axis
         ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.4),
                   fancybox=True, shadow=True, ncol=5)
-#   Save the plot 
-#    plt.show(block=False)
-### TO BE ABLE TO ZOOM USING THE SCRIPT, MAKE SURE TO USE plt.show(), so you can
-### SEE THE PLOT, ZOOM AND SAVE IT.
+# UNCOMMENT the lines below if you wonna save a figure
     plt.savefig(os.path.join(directory,'EatEachSCF.png'),dpi=300,bbox_inches='tight')
     plt.close()
+
 
 
 ######################################################
@@ -72,6 +74,11 @@ def GrabEnrg(directory):
 ######################################################
 def GrabDIIS(directory):
     filenameL=[]
+    filenameLL=[]
+    x_keys =  []
+    y_values= []
+    markers=[4,5,6,7,'s','P', '^',"*",'X']
+#
     for filename in os.listdir(directory):
         if filename.endswith(".log"):
             filenameL.append(filename)
@@ -85,40 +92,48 @@ def GrabDIIS(directory):
                     line = line.replace("D","e")
                     words = line.split()
                     DIIS = float(words[2])
-                    DIISL.append(DIIS) 
+                    DIISL.append(DIIS)
     #     print(DIISL)
-        xDIISL=list(range(len(DIISL)))
-        plt.plot(xDIISL,DIISL,label=os.path.splitext(os.path.split(file)[1])[0])
-#         style.use('seaborn-ticks')
-#         style.use('seaborn-poster') #sets the size of the charts
+            x_keys.append(list(range(len(DIISL))))
+#                 print(x_keys)
+            y_values.append(DIISL)
+            filenameLL.append(os.path.splitext(os.path.split(file)[1])[0])
+
+        zipped_list= list(zip(x_keys,y_values,markers,filenameLL))
+#         print(zipped3)
+    for x,y,z,fn in zipped_list:
+#         ax.plot(i,j, linestyle = '', marker=next(markers))
+        plt.plot(x,y,label=fn,marker=z,linewidth=0.5)
         style.use('seaborn-talk')
-#         style.use('ggplot')
-        plt.xlabel('Number of SCF iterations')
-        plt.ylabel('DIIS Error in (a.u.)')
-#       Making a small box under the plot for the legend
         ax = plt.subplot(111)
-        box = ax.get_position()    
+        box = ax.get_position()
         ax.set_position([box.x0, box.y0 + box.height * 0.1,
                          box.width, box.height * 0.9])
+        ax.set_xlabel('Number of SCF iterations')
+        ax.set_ylabel('DIIS Error in (a.u.)')
 
         # Put a legend below current axis
         ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.4),
                   fancybox=True, shadow=True, ncol=5)
-#    plt.show(block=False)
-### TO BE ABLE TO ZOOM USING THE SCRIPT, MAKE SURE TO USE plt.show(), so you can
-### SEE THE PLOT, ZOOM AND SAVE IT.
+# UNCOMMENT the lines below if you wonna save a figure
     plt.savefig(os.path.join(directory,'DIIS.png'),dpi=300,bbox_inches='tight')
     plt.close()
+
+
 
 #################################################
 # This Function will grab the RMSDP at each cycle
 #################################################
 def GrabRMSDP(directory):
     ref=(glob.glob('*ref.log'))
-    ref1 = "" 
-    for ele in ref:  
+    ref1 = ""
+    for ele in ref:
         ref1 += ele
     filenameL=[]
+    filenameLL=[]
+    x_keys =  []
+    y_values= []
+    markers=[4,5,6,7,'s','P', '^',"*",'X']
     for filename in os.listdir(directory):
         if filename.endswith(".log"):
             filenameL.append(filename)
@@ -126,7 +141,7 @@ def GrabRMSDP(directory):
     for file in filenameL:
         file=os.path.join(directory,file)
         if file == ref1:
-            continue    
+            continue
         RMSDPL=[]
         with open (file,'r') as f:
             for line in f:
@@ -135,35 +150,38 @@ def GrabRMSDP(directory):
                     line = line.replace("D","e")
                     words = line.split()
                     RMSDP = float(words[1])
-                    RMSDPL.append(RMSDP) 
+                    RMSDPL.append(RMSDP)
 #             print(RMSDPL)
-        xRMSDPL=list(range(len(RMSDPL)))
-        plt.plot(xRMSDPL,RMSDPL,label=os.path.splitext(os.path.split(file)[1])[0])    
-#         style.use('seaborn-poster') #sets the size of the charts
+            x_keys.append(list(range(len(RMSDPL))))
+#                 print(x_keys)
+            y_values.append(RMSDPL)
+            filenameLL.append(os.path.splitext(os.path.split(file)[1])[0])
+
+#                 print(y_values)
+
+#             print(RMSDPL)
+        zipped_list= list(zip(x_keys,y_values,markers,filenameLL))
+#         print(zipped_list)
+    for x,y,z,fn in zipped_list:
+#         ax.plot(i,j, linestyle = '', marker=next(markers))
+        plt.plot(x,y,label=fn,marker=z,linewidth=0.5)
         style.use('seaborn-talk')
-#         style.use('ggplot'
-        plt.xlabel('Number of SCF iterations')
-        plt.ylabel('RMSDP in (a.u.)')
-#       Making a small box under the plot for the legend
-        ax = plt.subplot(111)    
-        box = ax.get_position()    
+        ax = plt.subplot(111)
+        box = ax.get_position()
         ax.set_position([box.x0, box.y0 + box.height * 0.1,
                          box.width, box.height * 0.9])
+        ax.set_xlabel('Number of SCF iterations')
+        ax.set_ylabel('RMSDP in (a.u.)')
 
         # Put a legend below current axis
         ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.4),
                   fancybox=True, shadow=True, ncol=5)
 # UNCOMMENT the lines below if you wonna save a figure
-### TO BE ABLE TO ZOOM USING THE SCRIPT, MAKE SURE TO USE plt.show(), so you can
-### SEE THE PLOT, ZOOM AND SAVE IT.
     plt.savefig(os.path.join(directory,'RMSDP.png'),dpi=300,bbox_inches='tight')
     plt.close()
-
-
 #### RUNNING THE FUCNTIONS
 #### REMEBER TO PROVIDE THE PATH IF THE CODE AND THE FILES ARE NOT IN THE SAME FOLDER
 ####
-
 GrabEnrg(sys.argv[1])
 #####
 
@@ -172,6 +190,4 @@ GrabDIIS(sys.argv[1])
 
 GrabRMSDP(sys.argv[1])
 ####
-
-
 
